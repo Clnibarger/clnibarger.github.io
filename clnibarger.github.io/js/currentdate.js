2d53a73b03071fcd1f42f31bf736d81411dd2872
clnibarger <clnibarger@byui.edu>
2020-01-17 08:54:23 -0700

    hello

--- clnibarger.github.io/js/currentdate.js
+++
@@ -1,3 +0,0 @@
-const todaysdate = new Date();
-const options = {weekdays: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
